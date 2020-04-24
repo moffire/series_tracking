0de6477ea1c4bd216ef27493b66642a08e9f5124
moffire <alexeydudarev.spb@gmail.com>
2020-04-24 17:19:19 +0300

    changed method for top_rated movie list

---
+++ app/services/my_shows.rb
@@ -0,0 +1,92 @@
+require 'nokogiri'
+require 'open-uri'
+require 'cgi/util'
+
+class MyShows
+
+  BASE_URL = 'https://myshows.me'.freeze
+
+  attr_accessor :query
+
+  def initialize(query = nil)
+    @query = query
+  end
+
+  def parse_html(url = '')
+    Nokogiri::HTML(URI.open(BASE_URL + url))
+  rescue StandardError
+    logger.error 'Error 404. Page not found.'
+    nil
+  end
+
+  # hash of searched series or top rated movies if no query
+  def movies_list
+    return top_rated if query.nil?
+
+    content = {}
+    encoded_query = CGI.escape(@query)
+    html = parse_html("/search/?q=#{encoded_query}")
+    all_series = html.css('table.catalogTable').search('tr')
+    all_series[1...-1].each do |movie|
+      ru_title = movie.css('td > a')[0].text
+      content[:"#{ru_title}"] = {
+        en_title: movie.css('td > .catalogTableSubHeader')[0].text,
+        link: movie.css('td > a')[0]['href'],
+        movie_id: movie.css('td > a')[0]['href'][/\d+/],
+        watchers: movie.css('td')[2].text,
+        seasons: movie.css('td')[4].text,
+        year: movie.css('td')[5].text
+      }
+    end
+    content
+  rescue NoMethodError
+    logger.error "Can't apply selectors in '#{__method__}' method. DOM structure apparently was changed."
+  end
+
+  def seasons(movie_id)
+    seasons_list = {}
+
+    html = parse_html("/view/#{movie_id}/")
+    # list of all seasons
+    html.css('.col8 > form > .row[itemprop="season"]').reverse_each do |season|
+      season_number = season.css('.flat > a')[0].text
+      special_series_counter = 1
+
+      season.css('.widerCont > .infoList > li').reverse_each do |series|
+        series_number = series.css('._numb').text
+        if series_number.empty?
+          series_number = "#{special_series_counter} спец"
+          special_series_counter += 1
+        end
+
+        seasons_list["#{season_number}, #{series_number} серия"] = {
+          series_name: series.css('._name').text,
+          series_date: series.css('._date').text
+        }
+      end
+    end
+    seasons_list
+  rescue NoMethodError
+    logger.error "Can't apply selectors in '#{__method__}' method. DOM structure apparently was changed."
+  end
+
+  # top rated movies from main page
+  def top_rated
+    top_rated_list = {}
+    html = parse_html
+    html.css('.landing > .container > .row> .col3 > a')[1..8].each do |movie|
+      movie_id = movie['href'][/\d+/]
+      top_rated_list[:"#{movie_id}"] = {
+          movie_ru_title: movie.css('.fsHeader').text,
+          movie_en_title: movie.css('.cFadeLight').text,
+          movie_image_link: movie.search('._img').to_s[/(?<=\().+?(?=\))/],
+          movie_page_link: movie['href']
+      }
+    end
+    top_rated_list
+  rescue NoMethodError
+    logger.error "Can't apply selectors in '#{__method__}' method. DOM structure apparently was changed."
+  end
+end
+
+puts MyShows.new.top_rated
