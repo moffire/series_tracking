3ab026fa92571c31b570817453fb9f03fdbab349
moffire <alexeydudarev.spb@gmail.com>
2020-04-27 14:46:55 +0300

    added more info about movie

--- app/controllers/movie_info_controller.rb
+++ app/controllers/movie_info_controller.rb
@@ -2,6 +2,8 @@ class MovieInfoController < ApplicationController
   def show
     # query = params[:id]
     # @movie = MyShows.new.movie_info(query)
-    @movie = MyShows.new.movie_info(22410)
+    show = MyShows.new(22410)
+    @movie = show.movie_info
+    @series = show.episodes_list
   end
 end
