db3ca4c33c244c399e55c58611a9475aafd9cec2
moffire <alexeydudarev.spb@gmail.com>
2020-05-04 17:57:34 +0300

    added movie exteral_id

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
-  validates :image_url, :description, :country, :start_date, presence: true
+  validates :image_url, :description, :country, :start_date, :external_id, presence: true
   has_many :seasons
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_105018) do
+ActiveRecord::Schema.define(version: 2020_05_04_145508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_05_02_105018) do
     t.float "kinopoisk"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "external_id"
   end
 
   create_table "seasons", force: :cascade do |t|
