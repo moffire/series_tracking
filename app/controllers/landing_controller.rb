b96b2f000e9e52e0d3cad4ca1a709d75e0d2bb83
moffire <alexeydudarev.spb@gmail.com>
2020-05-16 19:38:54 +0300

    update templates for subscriptions and top rating movies

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,15 @@
 class LandingController < ApplicationController
   def index
+    @top_movies = Movie.all.order(imdb: :desc, kinopoisk: :desc).limit(8)
+  end
+
+  def top_imdb
     @top_movies = Movie.all.order(imdb: :desc).limit(8)
+    render 'landing/index'
+  end
+
+  def top_kinopoisk
+    @top_movies = Movie.all.order(kinopoisk: :desc).limit(8)
+    render 'landing/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'landing#index'
+
+  # header links
   get 'search', to: 'search#search_movies', as: 'search'
+  get 'top_imdb', to: 'landing#top_imdb', as: 'top_imdb'
+  get 'top_kp', to: 'landing#top_kinopoisk', as: 'top_kp'
+
+  # main content links
   get 'movie/:id', to: 'movie_info#movie', as: 'movie'
   get 'subscribe/:id', to: 'subscriptions#subscribe', as: 'subscribe'
   get 'unsubscribe/:id', to: 'subscriptions#unsubscribe', as: 'unsubscribe'
