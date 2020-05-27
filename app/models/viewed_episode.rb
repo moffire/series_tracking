3187053cc010346690aef3bbcd5f0177e971c637
moffire <alexeydudarev.spb@gmail.com>
2020-05-27 19:08:10 +0300

    added validations and removed unnecessary relations

--- app/models/viewed_episode.rb
+++ app/models/viewed_episode.rb
@@ -1,7 +1,9 @@
 class ViewedEpisode < ApplicationRecord
   belongs_to :user
-  belongs_to :subscription
   belongs_to :episode
+  validates :movie_id, presence: true
+  validates :episode_id, presence: true
+  validates :season_id, presence: true
   validates_uniqueness_of :episode_id, scope: :user_id
   before_create :set_status
 
