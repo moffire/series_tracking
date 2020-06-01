3d3c23d51759ee2b3cfec9af2d214647e5a5a461
moffire <alexeydudarev.spb@gmail.com>
2020-06-01 18:42:09 +0300

    added validation tests for Episode model

--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Episode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:number) }
+  it { should validate_presence_of(:title) }
 end
