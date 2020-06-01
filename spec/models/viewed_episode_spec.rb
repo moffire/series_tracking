c43b2ea6979ff6becfc1d648c28dd30b0df561d7
moffire <alexeydudarev.spb@gmail.com>
2020-06-01 20:02:43 +0300

    added validation tests for ViewedEpisode model

--- spec/models/viewed_episode_spec.rb
+++ spec/models/viewed_episode_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ViewedEpisode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation test' do
+    it { should validate_presence_of(:movie_id) }
+    it { should validate_presence_of(:episode_id) }
+    it { should validate_presence_of(:season_id) }
+    it { should belong_to(:episode).class_name('Episode') }
+    it { should belong_to(:user).class_name('User') }
+    it { should validate_uniqueness_of(:episode_id).scoped_to(:user_id) }
+  end
 end
