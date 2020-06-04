f680a8f31e6d8680dcca9e1473c6b5a409812265
moffire <alexeydudarev.spb@gmail.com>
2020-06-04 20:26:54 +0300

    added tests with factories

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,57 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
+
+  before(:example) do
+    @movie = create(:movie)
+    @subscriptions = @movie.subscriptions
+    @seasons = @movie.seasons
+    @episodes = @movie.episodes
+  end
+
   context 'validation tests' do
     it { should validate_presence_of(:country) }
     it { should_not validate_presence_of(:start_date) }
     it 'default value of start date should presence' do
-      movie = Movie.new
-      movie.valid?
-      movie.start_date.should eq('Неизвестно')
+      expect(@movie.start_date).to eq('Неизвестно')
     end
     it { should validate_presence_of(:external_id) }
-    it 'movie object must be valid' do
-      movie = Movie.new(ru_title: 'Ru-Title',
-                        country: 'Country',
-                        start_date: '22.06.2020',
-                        external_id: 12345)
-      expect(movie.valid?).to eq(true)
+    it 'movie object should be valid' do
+      expect(@movie.valid?).to eq(true)
     end
-    it 'movie object must be invalid' do
-      movie = Movie.new(ru_title: nil,
-                        en_title: nil,
-                        country: 'Country',
-                        start_date: '22.06.2020',
-                        external_id: 12345)
-      movie.valid?
-      expect(movie.errors.messages[:title]).to include('either one of ru_title or en_title must be presence')
+    it 'movie object should be invalid' do
+      @movie.ru_title = nil
+      @movie.en_title = nil
+      @movie.valid?
+      expect(@movie.errors.messages[:title]).to include('either one of ru_title or en_title must be presence')
     end
     it 'default value of image link should presence' do
-      movie = Movie.new
-      movie.valid?
-      expect(movie.image_url).to eq('https://dummyimage.com/600x400/f5f5f5/000000&text=No+image+=(')
+      expect(@movie.image_url).to eq('https://dummyimage.com/600x400/f5f5f5/000000&text=No+image+=(')
     end
     it 'should setup external image link' do
-      movie = Movie.new(image_url: 'external link')
-      movie.valid?
-      expect(movie.image_url).to eq('external link')
+      @movie.image_url = 'external link'
+      expect(@movie.image_url).to eq('external link')
     end
     it 'default value of description should presence' do
-      movie = Movie.new
-      movie.valid?
-      expect(movie.description).to eq('Описание недоступно')
+      expect(@movie.description).to eq('Описание недоступно')
     end
     it 'should setup external description' do
-      movie = Movie.new(description: 'lorem ipsum')
-      movie.valid?
-      expect(movie.description).to eq('lorem ipsum')
+      @movie.description = 'lorem ipsum'
+      expect(@movie.description).to eq('lorem ipsum')
     end
   end
-  
+
   context 'relation tests' do
     it { should have_many(:seasons) }
     it { should have_many(:episodes).through(:seasons) }
     it { should have_many(:subscriptions) }
     it { should have_many(:viewed_episodes) }
+    it 'should have related subscriptions' do
+      expect(@subscriptions.count).to eq(2)
+    end
+    it 'should have related seasons' do
+      expect(@seasons.count).to eq(2)
+    end
+    it 'should have related episodes' do
+      expect(@episodes.count).to eq(4)
+    end
   end
 end
