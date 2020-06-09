03577847d05e86124b7bb6ace06c0065dabaacc6
moffire <alexeydudarev.spb@gmail.com>
2020-06-09 21:17:43 +0300

    unviewed episodes counter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :unviewed_episodes_count
+  
+  
+  private
+  
+  def unviewed_episodes_count
+    if user_signed_in?
+      @unviewed_count = 0
+      current_user.subscriptions.each do |subscription|
+        @unviewed_count += subscription.movie.episodes.count - subscription.movie.viewed_episodes.count
+      end
+    end
+  end
 end
